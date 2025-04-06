c10bda47679b8991d18eb2e4bef846882039b077
david de la torre campo <datocam98@gmail.com>
2025-04-06 20:21:32 +0200

    correction

--- app/src/main/java/com/example/hooksounds_tfg/data/remote/ServiceBuilder.kt
+++ app/src/main/java/com/example/hooksounds_tfg/data/remote/ServiceBuilder.kt
@@ -4,10 +4,5 @@ object ServiceBuilder {
 
     private const val BASE_URL = "https://www.hooksounds.com/edd-api/"
     private const val AUTH_TOKEN = "a13bfad9c4535d7b15a803c8383e3a8c"
-
-
-
-
-
     val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
